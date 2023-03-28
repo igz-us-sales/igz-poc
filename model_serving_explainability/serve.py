b08e967b409c26cc4212750ff04ce2df12badbff
Nick Schenone <nschenone16@gmail.com>
2023-03-28 16:30:57 +0000

    First commit

---
+++ model_serving_explainability/serve.py
@@ -0,0 +1,26 @@
+import mlrun
+
+from cloudpickle import load
+from typing import List
+import numpy as np
+import shap
+
+class ClassifierModelSHAP(mlrun.serving.V2ModelServer):
+    def load(self):
+        """load and initialize the model and/or other elements"""
+        model_file, extra_data = self.get_model(".pkl")
+        self.model = load(open(model_file, "rb"))
+        self.explainer = shap.TreeExplainer(self.model)
+
+    def predict(self, body: dict) -> List:
+        """Generate model predictions from sample."""
+        feats = np.asarray(body["inputs"])
+        result: np.ndarray = self.model.predict(feats)
+        return result.tolist()
+    
+    def explain(self, body: dict) -> List:
+        """Generate model explaination from sample"""
+        feats = np.asarray(body["inputs"])
+        result: np.ndarray = self.model.predict(feats)
+        shap_values = self.explainer.shap_values(feats)
+        return shap_values[result.argmax()].tolist()
